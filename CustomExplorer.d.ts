ecd9bf780dfbfc0975c80d43dc171fa2bf17d01b
Brian Joseph Petro <brian@smartconnections.app>
2024-12-05 18:57:29 -0500

    up

--- CustomExplorer.d.ts
+++ CustomExplorer.d.ts
@@ -1,32 +1,3 @@
-import { ComponentType } from "preact"
-import { StaticResources } from "../quartz/util/resources"
-import { QuartzPluginData } from "../quartz/plugins/vfile"
-import { GlobalConfiguration } from "../quartz/cfg"
-import { Node } from "hast"
-import { BuildCtx } from "../quartz/util/ctx"
+import { QuartzComponent } from '../quartz/components/types'
 
-export type QuartzComponentProps = {
-  ctx: BuildCtx
-  externalResources: StaticResources
-  fileData: QuartzPluginData
-  cfg: GlobalConfiguration
-  children: (QuartzComponent | JSX.Element)[]
-  tree: Node
-  allFiles: QuartzPluginData[]
-  displayClass?: "mobile-only" | "desktop-only"
-} & JSX.IntrinsicAttributes & {
-    [key: string]: any
-  }
-
-export type QuartzComponent = ComponentType<QuartzComponentProps> & {
-  css?: string
-  beforeDOMLoaded?: string
-  afterDOMLoaded?: string
-}
-
-export type QuartzComponentConstructor<Options extends object | undefined = undefined> = (
-  opts: Options,
-) => QuartzComponent
-
-declare const createExplorer: QuartzComponentConstructor
-export default createExplorer 
+export default function createExplorer(userOpts?: any): QuartzComponent 

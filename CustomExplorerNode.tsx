452d967cf421051bc0e6d8ca441d4fc005e7a475
Brian Joseph Petro <brian@smartconnections.app>
2024-12-11 20:14:23 -0500

    fix

--- CustomExplorerNode.tsx
+++ CustomExplorerNode.tsx
@@ -215,7 +215,7 @@ export function ExplorerNode({ node, opts, fullPath, fileData }: ExplorerNodePro
             </div>
           )}
           {/* Recursively render children of folder */}
-          <div class={`folder-outer ${(node.depth === 0) || isDefaultOpen || fileData.filePath?.includes(node.displayName) ? "open" : ""}`}>
+          <div class={`folder-outer ${(node.depth === 0) || isDefaultOpen || fileData.slug?.includes(node.name) ? "open" : ""}`}>
             <ul
               // Inline style for left folder paddings
               style={{
